28c7ad52da7d25bda3a80951cd5d9220c36b8e03
Andrey Prokopenko <aprokop@sandia.gov>
2015-02-12 14:45:40 -0700

    backup: recycle oldest snapshot

--- backup.snapshots.sh
+++ backup.snapshots.sh
@@ -9,14 +9,17 @@
 
 unset PATH	# avoid accidental use of $PATH
 
+n=4
+
 # ------------- system commands used by this script --------------------
-ID=/usr/bin/id;
-ECHO=/bin/echo;
-RM=/bin/rm;
-MV=/bin/mv;
-CP=/bin/cp;
-TOUCH=/bin/touch;
-RSYNC=/usr/bin/rsync;
+CP=/bin/cp
+ECHO=/bin/echo
+ID=/usr/bin/id
+MV=/bin/mv
+RM=/bin/rm
+RSYNC=/usr/bin/rsync
+SEQ=/usr/bin/seq
+TOUCH=/bin/touch
 
 # ------directory names and labels associated with snapshot frequency
 USERNAME=aprokop
@@ -36,36 +39,35 @@ if (( `$ID -u` != 0 )); then { $ECHO "Sorry, must be root.  Exiting..."; exit; }
 # rotating snapshots
 # Script creates 6 incremental back-ups
 # From newest to oldest backups names:
-# 	$NAME
-# 	$NAME.1
-# 	$NAME.2
-# 	$NAME.3
-# 	$NAME.4
-# 	$NAME.5
-
-# step 0
-# Delete oldest snapshot
-if [ -d $SNAPSHOT_RW/$NAME.5 ]; then
-    $RM -rf $SNAPSHOT_RW/$NAME.5
-fi;
+#   $NAME
+#   $NAME.1
+#   $NAME.2
+#   $NAME.3
+#   $NAME.4
+#   $NAME.5
 
 # step 1
-# shift middle snapshots by one increment
-n=4
-for i in 4 3 2 1; do
-    DEST=$NAME.$((n+1))
-    SRC=$NAME.$n
-    if [ -d $SNAPSHOT_RW/$SRC ] ; then			\
-        $MV $SNAPSHOT_RW/$SRC $SNAPSHOT_RW/$DEST ;	\
-    fi;
-    (( n -= 1))
+# shift middle snapshots by one increment, recycle oldest
+if [ -d $SNAPSHOT_RW/$NAME.$((n+1)) ]; then
+    $MV $SNAPSHOT_RW/$NAME.$((n+1)) $SNAPSHOT_RW/$NAME.tmp
+fi
+for i in `$SEQ $n -1 0`; do
+    SRC=$NAME.$i
+    DEST=$NAME.$((i+1))
+    if [ -d $SNAPSHOT_RW/$SRC ] ; then
+        $MV $SNAPSHOT_RW/$SRC $SNAPSHOT_RW/$DEST
+    fi
 done
+if [ -d $SNAPSHOT_RW/$NAME.tmp ]; then
+    $MV $SNAPSHOT_RW/$NAME.tmp $SNAPSHOT_RW/$NAME.0
+fi
 
-# step 2: make a hard-link-only (except for dirs) copy of the latest snapshot,
-# if that exists
-if [ -d $SNAPSHOT_RW/$NAME ] ; then
-    $CP -al $SNAPSHOT_RW/$NAME $SNAPSHOT_RW/$NAME.1 ;
-fi;
+# step 2
+# make a hard-link-only (except for dirs) copy of the latest snapshot, if that
+# exists
+if [ -d $SNAPSHOT_RW/$NAME.1 ] ; then
+    $CP -alf $SNAPSHOT_RW/$NAME.1/. $SNAPSHOT_RW/$NAME.0
+fi
 
 # step 3: rsync from the system into the latest snapshot (notice that
 # rsync behaves like cp --remove-destination by default, so the destination
@@ -77,4 +79,4 @@ $RSYNC					\
     $HOME_DIR $SNAPSHOT_RW/$NAME ;
 
 # step 4: update the mtime to reflect the snapshot time
-$TOUCH $SNAPSHOT_RW/$NAME ;
+$TOUCH $SNAPSHOT_RW/$NAME.0
