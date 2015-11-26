6f3211a6428a84bc83deea4e2e7247c6cbda4c58
Andrey Prokopenko <aprokop@sandia.gov>
2015-11-26 02:00:38 -0700

    pers.sh: exclude git directory

--- pers.sh
+++ pers.sh
@@ -4,6 +4,7 @@ xstring=""
 for i in `grep path .personal/configs/.gitmodules  | sed 's/\s*path = //'`; do
     xstring="${xstring} --exclude=.personal/configs/$i"
 done
+xstring="${xstring} --exclude=.git"
 
 file="personal_`date +'%Y_%m_%d'`.tbz2"
 tar ${xstring} -cjvhf $file .personal/
