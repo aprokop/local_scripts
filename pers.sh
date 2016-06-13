d0c84361cf2cb0b3417d7fdb8403d58b57d2aa50
Andrey Prokopenko <aprokop@sandia.gov>
2016-06-13 00:47:08 -0600

    pers.sh: added a check for kdb

--- pers.sh
+++ pers.sh
@@ -6,6 +6,13 @@ for i in `grep path .personal/configs/.gitmodules  | sed 's/\s*path = //'`; do
 done
 xstring="${xstring} --exclude=.git"
 
+echo -n "Have you generated .kdb version? [yes/no] "
+read kdb
+
+if [ "$kdb" != "yes" ]; then
+    exit 1
+fi
+
 file="personal_`date +'%Y_%m_%d'`.tbz2"
 tar ${xstring} -cjvhf $file .personal/
 gpg -c --cipher-algo AES256 $file && rm $file
