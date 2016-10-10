d9aab60877b8a0564f84e8ae90ce3e1bd4831d8a
Andrey Prokopenko <prokopenkoav@ornl.gov>
2016-10-10 08:49:30 -0400

    pers.sh: update
    - Started using $PERSONAL instead of hard-coded
    - Added pre-cleaning up of okular_data
    - Exclude .vim/bundle now that most plugins are moving there
    
    I also learned that tar is very sensitive to exclude. If one does
        tar --exclude <smth> <path>
    and the <path> is a relative path but <smth> is an absolute path, it
    simply does not work. <smth> must be a subpath of <path>.

--- pers.sh
+++ pers.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
+PERSONAL="/home/prok/.personal"
+
 cd $HOME
+
 xstring=""
-for i in `grep path .personal/configs/.gitmodules  | sed 's/\s*path = //'`; do
-    xstring="${xstring} --exclude=.personal/configs/$i"
+for i in `grep path $PERSONAL/configs/.gitmodules  | sed 's/\s*path = //'`; do
+    xstring="${xstring} --exclude=$PERSONAL/configs/$i"
 done
 xstring="${xstring} --exclude=.git"
+xstring="${xstring} --exclude=$PERSONAL/configs/.vim/bundle"
+xstring="${xstring} --exclude=$PERSONAL/configs/.emacs.d/auto-save-list"
 
 echo -n "Have you generated .kdb version? [yes/no] "
 read kdb
-
 if [ "$kdb" != "yes" ]; then
     exit 1
 fi
 
+# Clean up okular
+cd $PERSONAL/okular_docdata
+./clean.sh
+cd -
+
 file="personal_`date +'%Y_%m_%d'`.tbz2"
-tar ${xstring} -cjvhf $file .personal/
+tar ${xstring} -cjvhf $file $PERSONAL
 gpg -c --cipher-algo AES256 $file && rm $file
