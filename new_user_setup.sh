7f87078a90cee8a563596926ff45d921be3eb7aa
Andrey Prokopenko <prokopenkoav@ornl.gov>
2017-05-08 12:00:05 -0400

    new_user_setup: minor update

--- new_user_setup.sh
+++ new_user_setup.sh
@@ -27,15 +27,15 @@ fi
 
 GIT_ADDRESS="git@github.com:"
 # GIT_ADDRESS="ssh://git@ssh.github.com:443/"
-git clone ${GIT_ADDRESS}aprokop/dotfiles         $P/configs
-git clone ${GIT_ADDRESS}aprokop/local_scripts    $P/scripts
-git clone ${GIT_ADDRESS}aprokop/package_configs  $P/trilinos_configs
+[ -d $P/configs ]          || git clone ${GIT_ADDRESS}aprokop/dotfiles         $P/configs
+[ -d $P/scripts ]          || git clone ${GIT_ADDRESS}aprokop/local_scripts    $P/scripts
+[ -d $P/trilinos_configs ] || git clone ${GIT_ADDRESS}aprokop/package_configs  $P/trilinos_configs
 
 # Initialize submodule
 cd $P/configs
 git submodule init
 GIT_ALLOW_PROTOCOL="hg:git:http:https:ssh" git submodule update
-cd -
+cd ~
 
 # Link to ~/bin
 mkdir -p $HOME/bin
