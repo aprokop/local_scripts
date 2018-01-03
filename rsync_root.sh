603b0c05ec796106f4324a0e36914d4ba1756a20
Andrey Prokopenko <prokopenkoav@ornl.gov>
2018-01-03 06:32:38 -0500

    rsync_root.sh: ignore docker directory

--- rsync_root.sh
+++ rsync_root.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 rsync -aAv \
-        --one-file-system \
-	--delete \
-	--exclude "/dev" \
-	--exclude "/home" \
-	--exclude "/media" \
-	--exclude "/mnt" \
-	--exclude "/proc" \
-	--exclude "/root/.ccache" \
-	--exclude "/usr/portage/distfiles" \
-	--exclude "/sys" \
-	--exclude "/tmp" \
-	--exclude "/var/log" \
-	--exclude "/var/tmp" \
-	\
-	/ rsync_root
+    --one-file-system \
+    --delete \
+    --exclude "/dev" \
+    --exclude "/home" \
+    --exclude "/media" \
+    --exclude "/mnt" \
+    --exclude "/proc" \
+    --exclude "/root/.ccache" \
+    --exclude "/usr/portage/distfiles" \
+    --exclude "/sys" \
+    --exclude "/tmp" \
+    --exclude "/var/lib/docker" \
+    --exclude "/var/log" \
+    --exclude "/var/tmp" \
+    \
+    / rsync_root
